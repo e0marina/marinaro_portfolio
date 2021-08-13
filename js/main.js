ebcbea6c0200aa28ab118cd2ab6b2e2ba0a19e66
e0marina <emilymarinaro@gmail.com>
2021-08-13 10:15:09 -0700

    updated resume and about me

--- js/main.js
+++ js/main.js
@@ -1,94 +1,92 @@
-"use strict";
+'use strict';
 
-$(window).on("load", function() {
+$(window).on('load', function () {
   /*------------------
 		Preloder
 	--------------------*/
-  $(".loader").fadeOut();
-  $("#preloder")
-    .delay(400)
-    .fadeOut("slow");
+  $('.loader').fadeOut();
+  $('#preloder').delay(400).fadeOut('slow');
 
   /*------------------
 		Isotope Filter
 	--------------------*/
-  var $container = $(".isotope_items");
+  var $container = $('.isotope_items');
   $container.isotope();
 
-  $(".portfolio-filter li").on("click", function() {
-    $(".portfolio-filter li").removeClass("active");
-    $(this).addClass("active");
-    var selector = $(this).attr("data-filter");
-    $(".isotope_items").isotope({
+  $('.portfolio-filter li').on('click', function () {
+    $('.portfolio-filter li').removeClass('active');
+    $(this).addClass('active');
+    var selector = $(this).attr('data-filter');
+    $('.isotope_items').isotope({
       filter: selector,
       animationOptions: {
         duration: 750,
-        easing: "linear",
-        queue: false
-      }
+        easing: 'linear',
+        queue: false,
+      },
     });
     return false;
   });
 });
 
-(function($) {
+(function ($) {
   /*------------------
   		HEADER
   	--------------------*/
-  var navMenu = $(".menu-list");
+  var navMenu = $('.menu-list');
   navMenu.onePageNav();
-  $(window).on("scroll resize", function(e) {
+  $(window).on('scroll resize', function (e) {
     if ($(this).scrollTop() > 70) {
-      $(".header-section").addClass("sticky");
+      $('.header-section').addClass('sticky');
     } else {
-      $(".header-section").removeClass("sticky");
+      $('.header-section').removeClass('sticky');
     }
     e.preventDefault();
   });
 
-  $(".responsive").on("click", function(event) {
-    $(".menu-list").slideToggle(400);
-    $(".header-section").toggleClass("bgc");
+  $('.responsive').on('click', function (event) {
+    $('.menu-list').slideToggle(400);
+    $('.header-section').toggleClass('bgc');
     event.preventDefault();
   });
 
-  $(".menu-list li a").on("click", function(event) {
+  $('.menu-list li a').on('click', function (event) {
     if ($(window).width() < 768) {
-      $(".menu-list").slideUp(400);
-      $(".header-section").removeClass("bgc");
+      $('.menu-list').slideUp(400);
+      $('.header-section').removeClass('bgc');
     }
   });
 
   /*------------------
 		TYPED JS
 	--------------------*/
-  $(".element").typed({
-    strings: ["Hi, I'm Emily", "Web Developer", "From Los Angeles"],
+  $('.element').typed({
+    strings: ["Hi, I'm Emily", 'Full Stack Developer', 'From Los Angeles'],
     typeSpeed: 10,
     loop: true,
-    backDelay: 2000
+    backDelay: 2000,
   });
 
   /*------------------
 		FOOTER
 	--------------------*/
-  var fh = $(".footer-section").height();
+  var fh = $('.footer-section').height();
   fh = fh + 140;
-  $(".main-warp").css("margin-bottom", fh);
+  $('.main-warp').css('margin-bottom', fh);
 
   /*------------------
 		PROGRESS BAR
 	--------------------*/
-  $(".progress-bar-style").each(function() {
-    var progress = $(this).data("progress");
-    var prog_width = progress + "%";
+  $('.progress-bar-style').each(function () {
+    var progress = $(this).data('progress');
+    var prog_width = progress + '%';
     if (progress <= 100) {
       $(this).append(
         '<div class="bar-inner" style="width:' +
           prog_width +
           '"><span>' +
           prog_width +
-          "</span></div>"
+          '</span></div>'
       );
     } else {
       $(this).append(
@@ -100,7 +98,7 @@ $(window).on("load", function() {
   /*------------------
 		OWL CAROUSEL
 	--------------------*/
-  $("#review-carousel").owlCarousel({
+  $('#review-carousel').owlCarousel({
     dots: false,
     nav: true,
     loop: true,
@@ -110,8 +108,8 @@ $(window).on("load", function() {
     autoplay: true,
     navText: [
       '<i class="fa fa-angle-left"></i>',
-      '<i class="fa fa-angle-right"></i>'
-    ]
+      '<i class="fa fa-angle-right"></i>',
+    ],
   });
 
   /*------------------
@@ -132,42 +130,42 @@ $(window).on("load", function() {
   /*------------------
 		CONTACT FORM
 	--------------------*/
-  $("#contact-form").on("submit", function() {
-    var send_btn = $("#send-form"),
+  $('#contact-form').on('submit', function () {
+    var send_btn = $('#send-form'),
       form = $(this),
       formdata = $(this).serialize(),
-      chack = $("#form-chack");
-    send_btn.text("Wait...");
+      chack = $('#form-chack');
+    send_btn.text('Wait...');
 
     function reset_form() {
-      $("#name").val("");
-      $("#email").val("");
-      $("#massage").val("");
+      $('#name').val('');
+      $('#email').val('');
+      $('#massage').val('');
     }
 
     $.ajax({
-      url: $(form).attr("action"),
-      type: "POST",
+      url: $(form).attr('action'),
+      type: 'POST',
       data: formdata,
-      success: function(text) {
-        if (text == "success") {
-          send_btn.addClass("done");
-          send_btn.text("Success");
-          setTimeout(function() {
+      success: function (text) {
+        if (text == 'success') {
+          send_btn.addClass('done');
+          send_btn.text('Success');
+          setTimeout(function () {
             reset_form();
-            send_btn.removeClass("done");
-            send_btn.text("Massage");
+            send_btn.removeClass('done');
+            send_btn.text('Massage');
           }, 2500);
         } else {
           reset_form();
-          send_btn.addClass("error");
-          send_btn.text("Error");
-          setTimeout(function() {
-            send_btn.removeClass("error");
-            send_btn.text("Massage");
+          send_btn.addClass('error');
+          send_btn.text('Error');
+          setTimeout(function () {
+            send_btn.removeClass('error');
+            send_btn.text('Massage');
           }, 5000);
         }
-      }
+      },
     });
     return false;
   });
